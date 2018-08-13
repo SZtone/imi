30cec65194188e1c8dd10e577011df4ec4dd7c72
宇润 <admin@yurunsoft.com>
2018-08-13 09:20:33 +0800

    修复问题

--- HttpDemo/MainServer/config/route.php
+++ HttpDemo/MainServer/config/route.php
@@ -12,42 +12,42 @@ class Test extends HttpController
 	}
 }
 return [
-	[
-		'controller'	=>	\ImiDemo\HttpDemo\MainServer\Controller\Test::class,
-		'method'		=>	'index',
-		'route'	=>	[
-			'url'	=>	'/test',
-			// 'method'	=>	'PUT',
-			// 'method'	=>	['GET', 'POST'],
-			// 'domain'	=>	'{name}.xxx.com',
-		],
-	],
-	[
-		'route'	=>	[
-			'url'	=>	'/callback1',
-		],
-		'callback'	=>	function(){
-			return RequestContext::get('response')->write('callback1');
-		},
-	],
-	[
-		'route'	=>	[
-			'url'	=>	'/callback2',
-		],
-		'callback'	=>	new RouteCallable('\Test', 'abc'),
-	],
-	[
-		'controller'	=>	'\ImiDemo\HttpDemo\MainServer\Controller\Test{$interface}',
-		'method'		=>	'{$interface}',
-		'route'	=>	[
-			'url'	=>	'/test/{interface}',
-		],
-	],
-	[
-		'controller'	=>	\ImiDemo\HttpDemo\MainServer\Controller\Test::class,
-		'method'		=>	'{$interface}',
-		'route'	=>	[
-			'url'	=>	'/{interface}',
-		],
-	],
+	// [
+	// 	'controller'	=>	\ImiDemo\HttpDemo\MainServer\Controller\Test::class,
+	// 	'method'		=>	'index',
+	// 	'route'	=>	[
+	// 		'url'	=>	'/test',
+	// 		// 'method'	=>	'PUT',
+	// 		// 'method'	=>	['GET', 'POST'],
+	// 		// 'domain'	=>	'{name}.xxx.com',
+	// 	],
+	// ],
+	// [
+	// 	'route'	=>	[
+	// 		'url'	=>	'/callback1',
+	// 	],
+	// 	'callback'	=>	function(){
+	// 		return RequestContext::get('response')->write('callback1');
+	// 	},
+	// ],
+	// [
+	// 	'route'	=>	[
+	// 		'url'	=>	'/callback2',
+	// 	],
+	// 	'callback'	=>	new RouteCallable('\Test', 'abc'),
+	// ],
+	// [
+	// 	'controller'	=>	'\ImiDemo\HttpDemo\MainServer\Controller\Test{$interface}',
+	// 	'method'		=>	'{$interface}',
+	// 	'route'	=>	[
+	// 		'url'	=>	'/test/{interface}',
+	// 	],
+	// ],
+	// [
+	// 	'controller'	=>	\ImiDemo\HttpDemo\MainServer\Controller\Test::class,
+	// 	'method'		=>	'{$interface}',
+	// 	'route'	=>	[
+	// 		'url'	=>	'/{interface}',
+	// 	],
+	// ],
 ];
